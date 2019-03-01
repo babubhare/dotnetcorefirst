3fa2b156fa3af9de9a98d4b4fb532d0a76b18d47
babuchandranna <babu.chandranna@powerschool.com>
2019-03-01 10:56:50 +0530

    no ssl

--- dotnetcorewebapplication/dotnetcorewebapplication/Startup.cs
+++ dotnetcorewebapplication/dotnetcorewebapplication/Startup.cs
@@ -48,7 +48,7 @@ namespace dotnetcorewebapplication
 				app.UseHsts();
 			}
 
-			app.UseHttpsRedirection();
+			//app.UseHttpsRedirection();
 			app.UseStaticFiles();
 			app.UseCookiePolicy();
 
